524cae606b3e4e2366fdf94ea70aca9d89d4def0
surenganne <surendra.ganne@gmail.com>
2024-05-09 18:51:03 +0530

    Content and file changes 9th May

--- web/app/components/tools/contribute.tsx
+++ web/app/components/tools/contribute.tsx
@@ -2,14 +2,14 @@
 import type { FC } from 'react'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Heart02 } from '../base/icons/src/vender/solid/education'
 import { BookOpen01 } from '../base/icons/src/vender/line/education'
+import { Heart02 } from '../base/icons/src/vender/solid/education'
 
 const Contribute: FC = () => {
   const { t } = useTranslation()
 
   return (
-    <div className='shrink-0 p-2'>
+    <div className='shrink-0 p-2 hidden'>
       <div className='inline-block p-2 bg-white shadow-lg rounded-lg'>
         <Heart02 className='w-3 h-3 text-[#EE46BC]' />
       </div>
